671e438ba15acee1509b9b636fdb0c7c0b5e99e1
PIT <67591546+pit1107@users.noreply.github.com>
2022-04-06 07:24:10 +0700

    Add files via upload

---
+++ DubaothoitietAPI/js/index.js
@@ -0,0 +1,119 @@
+const APP_ID = 'ef44b9d5e0c9d71053ccac067026af84';
+const DEFAULT_VALUE = '--';
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherState = document.querySelector('.weather-state');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then(async res => {
+            const data = await res.json();
+            console.log('[Search Input]', data);
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+        });
+});
+
+//Tro ly ao
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'vi-VI';
+recognition.continuous = false;
+
+const microphone = document.querySelector('.microphone');
+
+const synth = window.speechSynthesis;
+
+const speak = (text) => {
+    if(synth.speaking){
+        console.error('Busy. Speaking...');
+        return;
+    }
+
+    const utter = new SpeechSynthesisUtterance(text);
+
+    utter.onend = () => {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utter.onerror = (err) => {
+        console.error('SpeechSynthesisUtterance.onerror', err);
+    }
+
+    synth.speak(utter);
+}
+
+const handleVoice = (text) => {
+    console.log('text', text);
+
+    //Tìm kiếm bằng giọng nói
+    const handleText = text.toLowerCase();
+    if (handleText.includes('thời tiết tại')){
+        const location = handleText.split('tại')[1].trim();
+
+        console.log('location', location);
+        searchInput.value = location;
+        const changeEvent = new Event('change');
+        searchInput.dispatchEvent(changeEvent);
+    }
+
+    //Thay đổi màu nền bằng giọng nói
+    const container = document.querySelector('.container');
+    if (handleText.includes('thay đổi màu nền')) {
+        const color = handleText.split('màu nền')[1].trim();
+        container.style.background = color;
+        return;
+    }
+
+    if(handleText.includes('màu nền mặc định')){
+        container.style.background = '';
+        return;
+    }
+
+    //Hỏi thời gian hiện tại bằng giọng nói
+    if (handleText.includes('mấy giờ')){
+        const textToSpeech = `${moment().hours()} hours ${moment().minutes()} minutes`;
+        speak(textToSpeech);
+        return;
+    }
+}
+
+microphone.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    recognition.start();
+    microphone.classList.add('recording');
+});
+
+recognition.onspeechend = () => {
+    recognition.stop();
+    microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+    console.error(err);
+    microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
+}
